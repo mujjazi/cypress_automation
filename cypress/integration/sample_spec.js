6db7117df1a700608ec0723a83bf25086a6a4a61
mujjazi <13besemmehdi@seecs.edu.pk>
2020-07-06 14:18:30 +0500

    Login Case Completed

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,13 +1,22 @@
-describe('Registration Test', () => {
-    it('Email Registration', () => {
+describe('Portal Creation', () => {
+    it('Portal', () => {
+      cy.viewport(1024, 768)
+
       cy.visit('https://apimatic.io/')
-  
-      cy.pause()
-  
-      cy.contains('Sign Up for Free').click()
+      //cy.get('//*[@id="gatsby-focus-wrapper"]/div/nav/div/button').click();
+      cy.get('a[href*="login"]').click()
+
+      cy.get('#Email').click();
+      cy.get('#Email').type('mujtaba.mehdi@apimatic.io');
+      
+      cy.get('#js-onboarding-password-field').click();
+      cy.get('#js-onboarding-password-field').type('Pakistan@123');
+
+      // simulate clicking submit
+      cy.get('button[type=submit]').click();
   
       // Should be on a new URL which includes '/commands/actions'
-      cy.url().should('include', '/account/register?email=')
+      cy.url().should('include', 'apimatic.io/dashboard')
   
       // Get an input, type into it and verify that the value has been updated
       //cy.get('//*[@id="editor"]/div/div/div/div[3]/div/div/div[1]/div/div[1]/div/div[6]/div[2]/span').click()
