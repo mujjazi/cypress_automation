0c85cf77bf75320b5e5816cb486a7a716bddd9cc
mujjazi <13besemmehdi@seecs.edu.pk>
2020-07-07 06:30:29 +0500

    Delete API and Import Added

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -8,6 +8,7 @@ describe('Portal Creation', () => {
 
       cy.get('#Email').click();
       cy.get('#Email').type('mujtaba.mehdi@apimatic.io');
+
       
       cy.get('#js-onboarding-password-field').click();
       cy.get('#js-onboarding-password-field').type('Pakistan@123');
@@ -18,23 +19,40 @@ describe('Portal Creation', () => {
       // Should be on a new URL which includes '/commands/actions'
       cy.url().should('include', 'apimatic.io/dashboard')
 
+      cy.get("body").then($body => {
+        if ($body.find('[ng-click="$ctrl.skipOnboarding()"]').length > 0) {   //evaluates as true
+          cy.get('[ng-click="$ctrl.skipOnboarding()"]').click();
+        }
+    });
+
+    cy.get("body").then($body => {
+      if ($body.find('.delete-api').length > 0) {   //evaluates as true
+        cy.get('.delete-api').first().click()
+        cy.get('[ng-hide="allSelected"]').contains('Select All').click()
+        cy.get('[ng-click="screen_1_primaryButtonClicked()"]').first().click()
+        cy.get('[ng-click="DeleteAPIVersion()"]').first().click()
+
+        cy.wait(4000) // wait for 2 seconds
+      }
+  });
+
+    
+
       cy.get('.import-api').click();
       cy.wait(4000) // wait for 2 seconds
 
-     // cy.get('input[name=importUrl]').eq(1).click();
-      //cy.get('importUrl').click();
-      cy.get('input[name=importUrl]').eq(1).type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/bookingpal.json-Swagger20.json');
+      cy.get('div.modal-body').find('input[ng-model="importUrl"]').type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/bookingpal.json-Swagger20.json');
 
       // simulate clicking import
       //cy.get('[@id="apiImportModal"]/div/div[3]/div/form/div[2]/input[1]').click();
       cy.wait(4000) // wait for 2 seconds
 
-      cy.get('[name="importApiForm"]').eq(1).submit()   // Submit a form
+      cy.get('[name="importApiForm"]').eq(0).submit()   // Submit a form
       
-      cy.wait(4000)
+      cy.wait(5000)
 
-      cy.get('[data-dismiss="modal"]').click();
-      
+      //cy.get('[data-dismiss="modal"]').click();
+      cy.click(500, 500)
 
       cy.get('.api-card').contains('Channel Management API');
   
