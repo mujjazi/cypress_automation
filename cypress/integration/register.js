5c2d2cb06b2b70861f90d7884eb9f3ce3897c4e4
mujjazi <13besemmehdi@seecs.edu.pk>
2020-07-11 10:56:56 +0500

    Added Login and Register Tests

---
+++ cypress/integration/register.js
@@ -0,0 +1,41 @@
+
+
+
+function randomnum (maxInt) {
+    return Math.floor(Math.random() * (maxInt + 1));
+  }
+
+
+describe('Register a new user', () => {
+    it('Registers a new user', () => {
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+            return false;
+          });
+    var name = 'mujtaba'
+    var username = 'mujtaba.mehdi'
+    var domain =  '@apimatic.io'
+    var rand = '+' + randomnum(10000)
+    var randemail = username+rand+domain
+    var number =  123456789
+    var password = 'Pakistan@123'
+    
+    
+    cy.visit('https://www.apimatic.io/')  
+    cy.get('a[href*="register"]').first().click()
+    cy.get('#signUpInputName').type(name)
+    cy.get('#signUpInputEmail').type(randemail)
+    cy.get('#signUpInputPassword').type(password)
+    cy.get('#signUpInputCompany').type(name)
+    cy.get('#signUpInputPhoneNumber').type(number)
+    cy.get('[data-sitekey="6LdiJaYZAAAAALVNZs7si7T8k1YtVZHNc_auC4Hx"]').click();
+    cy.wait(5000);
+
+    cy.get("body").then($body => {
+        if ($body.find('.content').length > 0) {
+    cy.get('.content').contains('Please check your email and confirm your email address');
+}
+});
+
+    })
+})
--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,32 +1,11 @@
-describe('Portal Creation', () => {
-    it('Portal', () => {
+describe('Login and Import Test', () => {
+    it('Logs in and Imports a API spec', () => {
 
       var randomInteger = function(maxInt) {
         return Math.floor(Math.random() * (maxInt + 1));
     };
-
-      const user = {
-        name: 'Automated Test',
-        username: 'mujtaba.mehdi',
-        domain:  '@apimatic.io',
-        rand: randomInteger(9),
-        email: user.username+user.rand+user.domain,
-        password: 'Pakistan@123',
-        number: 123456789
-      }
-      cy.viewport(1920,1080)
-
-      cy.visit('https://apimatic.io/')
       
-      cy.get('a[href*="register"]').click()
-      cy.get('#signUpInputName').type(user.name)
-      cy.get('#signUpInputEmail').type(user.email)
-      cy.get('#signUpInputPassword').type(user.password)
-      cy.get('#signUpInputCompany').type(user.name)
-      cy.get('#signUpInputPhoneNumber').type(user.number)
-      cy.get('[data-sitekey="6LdiJaYZAAAAALVNZs7si7T8k1YtVZHNc_auC4Hx"]').click
-      
-      //cy.get('//*[@id="gatsby-focus-wrapper"]/div/nav/div/button').click();
+      cy.visit('https://www.apimatic.io/')  
       cy.get('a[href*="login"]').click()
 
       cy.get('#Email').click();
@@ -41,6 +20,18 @@ describe('Portal Creation', () => {
   
       // Should be on a new URL which includes '/commands/actions'
       cy.url().should('include', 'apimatic.io/dashboard')
+      cy.wait(5000)
+
+      cy.get("body").then($body => {
+        if ($body.find('.delete-api').length > 0) {   //evaluates as true
+          cy.get('.delete-api').first().click()
+          cy.get('[ng-hide="allSelected"]').contains('Select All').click()
+          cy.get('[ng-click="screen_1_primaryButtonClicked()"]').first().click()
+          cy.get('[ng-click="DeleteAPIVersion()"]').first().click()
+  
+          cy.wait(4000) // wait for 2 seconds
+        }
+    });
 
       cy.get("body").then($body => {
         if ($body.find('[ng-click="$ctrl.skipOnboarding()"]').length > 0) {   //evaluates as true
@@ -48,41 +39,22 @@ describe('Portal Creation', () => {
         }
     });
 
-    cy.get("body").then($body => {
-      if ($body.find('.delete-api').length > 0) {   //evaluates as true
-        cy.get('.delete-api').first().click()
-        cy.get('[ng-hide="allSelected"]').contains('Select All').click()
-        cy.get('[ng-click="screen_1_primaryButtonClicked()"]').first().click()
-        cy.get('[ng-click="DeleteAPIVersion()"]').first().click()
 
-        cy.wait(4000) // wait for 2 seconds
-      }
-  });
 
       cy.get('.import-api').click();
       cy.wait(4000) // wait for 2 seconds
 
       cy.get('div.modal-body').find('input[ng-model="importUrl"]').type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/bookingpal.json-Swagger20.json');
 
-      // simulate clicking import
-      //cy.get('[@id="apiImportModal"]/div/div[3]/div/form/div[2]/input[1]').click();
-      cy.wait(4000) // wait for 2 seconds
+     
+      cy.wait(4000) // wait for 4 seconds
 
       cy.get('[name="importApiForm"]').eq(0).submit()   // Submit a form
       
-      cy.wait(2000)
+      cy.wait(8000)
 
-     // cy.get("body").then($body => {
-      //cy.get('body').click(500, 500)
-      //});
-      //cy.get('[data-dismiss="modal"]').click();
-  
 
-      cy.get('.api-card').contains('Channel Management API');
-  
-      // Get an input, type into it and verify that the value has been updated
-      //cy.get('//*[@id="editor"]/div/div/div/div[3]/div/div/div[1]/div/div[1]/div/div[6]/div[2]/span').click()
-      //cy.get('.blockbuilder-content-tool-icon').click();
+      cy.get('.api-card').should('include','Channel Management API');
 
     })
   })
