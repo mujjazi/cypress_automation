bd8a220480162f3c9575e0fdc309d62838bc35a2
mujjazi <13besemmehdi@seecs.edu.pk>
2020-07-12 08:06:21 +0500

    Added a new spec for DX Portal

--- cypress/integration/dxportal.js
+++ cypress/integration/dxportal.js
@@ -1,60 +1,45 @@
-describe('Login and Import Test', () => {
-    it('Logs in and Imports a API spec', () => {
 
-      var randomInteger = function(maxInt) {
-        return Math.floor(Math.random() * (maxInt + 1));
-    };
+describe('DX Portal Test', () => {
+    it('Logs in and Generates a DX Portal', () => {
       
+      const xpath = require('cypress-xpath')
+
       cy.visit('https://www.apimatic.io/')  
       cy.get('a[href*="login"]').click()
-
       cy.get('#Email').click();
       cy.get('#Email').type('mujtaba.mehdi@apimatic.io');
-
-      
       cy.get('#js-onboarding-password-field').click();
       cy.get('#js-onboarding-password-field').type('Pakistan@123');
-
       // simulate clicking submit
       cy.get('button[type=submit]').click();
-  
-      // Should be on a new URL which includes '/commands/actions'
+      // Should be on a new URL which includes '/dashboard'
       cy.url().should('include', 'apimatic.io/dashboard')
       cy.wait(5000)
 
+      //Check for already present api card and delete it if present
       cy.get("body").then($body => {
         if ($body.find('.delete-api').length > 0) {   //evaluates as true
           cy.get('.delete-api').first().click()
           cy.get('[ng-hide="allSelected"]').contains('Select All').click()
           cy.get('[ng-click="screen_1_primaryButtonClicked()"]').first().click()
           cy.get('[ng-click="DeleteAPIVersion()"]').first().click()
-  
           cy.wait(4000) // wait for 2 seconds
         }
     });
-
+// check for an already present welcome tour and skip it
       cy.get("body").then($body => {
         if ($body.find('[ng-click="$ctrl.skipOnboarding()"]').length > 0) {   //evaluates as true
           cy.get('[ng-click="$ctrl.skipOnboarding()"]').click();
         }
     });
 
-
-
-      cy.get('.import-api').click();
-      cy.wait(4000) // wait for 2 seconds
-
-      cy.get('div.modal-body').find('input[ng-model="importUrl"]').type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/bookingpal.json-Swagger20.json');
-
-     
-      cy.wait(4000) // wait for 4 seconds
-
-      cy.get('[name="importApiForm"]').eq(0).submit()   // Submit a form
-      
-      cy.wait(8000)
-
-
-      cy.get('.api-card').should('include','Channel Management API');
-
-    })
-  })
+        cy.contains('Generate Portal').click();
+        cy.get('#apiDescriptionURL').type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/swagger.json');
+        cy.wait(2000) // wait for 4 seconds
+        cy.get('[name="dxPortalImportForm"]').eq(0).submit()   // Submit a form
+        cy.wait(8000) // wait for 8 seconds
+        cy.xpath("//a[@class='cancelButton btn btn-default'][contains(text(),'Proceed')]").click()
+        cy.contains('Portal Generated Successfully').should('be.visible')              // Assert that el is visible
+        
+    });
+});
---
+++ cypress/integration/login-portal.js
@@ -0,0 +1,44 @@
+describe('Login and Import Test', () => {
+    it('Logs in and Imports a API spec', () => {
+      
+      const xpath = require('cypress-xpath')
+
+      cy.visit('https://www.apimatic.io/')  
+      cy.get('a[href*="login"]').click()
+      cy.get('#Email').click();
+      cy.get('#Email').type('mujtaba.mehdi@apimatic.io');
+      cy.get('#js-onboarding-password-field').click();
+      cy.get('#js-onboarding-password-field').type('Pakistan@123');
+      // simulate clicking submit
+      cy.get('button[type=submit]').click();
+      // Should be on a new URL which includes '/dashboard'
+      cy.url().should('include', 'apimatic.io/dashboard')
+      cy.wait(5000)
+
+      //Check for already present api card and delete it if present
+      cy.get("body").then($body => {
+        if ($body.find('.delete-api').length > 0) {   //evaluates as true
+          cy.get('.delete-api').first().click()
+          cy.get('[ng-hide="allSelected"]').contains('Select All').click()
+          cy.get('[ng-click="screen_1_primaryButtonClicked()"]').first().click()
+          cy.get('[ng-click="DeleteAPIVersion()"]').first().click()
+          cy.wait(4000) // wait for 2 seconds
+        }
+    });
+// check for an already present welcome tour and skip it
+      cy.get("body").then($body => {
+        if ($body.find('[ng-click="$ctrl.skipOnboarding()"]').length > 0) {   //evaluates as true
+          cy.get('[ng-click="$ctrl.skipOnboarding()"]').click();
+        }
+    });
+
+      cy.get('.import-api').click();
+      cy.wait(4000) // wait for 2 seconds
+      cy.xpath("//div[@class='modal-content']//input[@placeholder='Recommended for relative path references e.g., imports and includes']").type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/bookingpal.json-Swagger20.json');
+      cy.wait(2000) // wait for 4 seconds
+      cy.get('[name="importApiForm"]').eq(0).submit()   // Submit a form
+      cy.wait(12000)
+      //cy.get('.api-card').should('include','Channel Management API');
+      cy.contains('Channel Management API').should('be.visible') 
+    })
+  })
