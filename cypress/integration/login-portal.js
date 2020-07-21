88919925a7efb7f9a848fd69dccdd33b5f45a176
mujjazi <13besemmehdi@seecs.edu.pk>
2020-07-21 11:34:58 +0500

    Added dynamic waits

--- cypress/integration/login-portal.js
+++ cypress/integration/login-portal.js
@@ -13,7 +13,7 @@ describe('Login and Import Test', () => {
       cy.get('button[type=submit]').click();
       // Should be on a new URL which includes '/dashboard'
       cy.url().should('include', 'apimatic.io/dashboard')
-      cy.wait(5000)
+      cy.wait(6000)
 
       //Check for already present api card and delete it if present
       cy.get("body").then($body => {
@@ -22,7 +22,7 @@ describe('Login and Import Test', () => {
           cy.get('[ng-hide="allSelected"]').contains('Select All').click()
           cy.get('[ng-click="screen_1_primaryButtonClicked()"]').first().click()
           cy.get('[ng-click="DeleteAPIVersion()"]').first().click()
-          cy.wait(4000) // wait for 2 seconds
+          cy.wait(6000) // wait for 2 seconds
         }
     });
 // check for an already present welcome tour and skip it
@@ -33,11 +33,11 @@ describe('Login and Import Test', () => {
     });
 
       cy.get('.import-api').click();
-      cy.wait(4000) // wait for 2 seconds
+      cy.wait(5000) // wait for 2 seconds
       cy.xpath("//div[@class='modal-content']//input[@placeholder='Recommended for relative path references e.g., imports and includes']").type('https://github.com/mujjazi/Cypress_Jenkins/blob/master/bookingpal.json-Swagger20.json');
       cy.wait(2000) // wait for 4 seconds
       cy.get('[name="importApiForm"]').eq(0).submit()   // Submit a form
-      cy.wait(12000)
+      cy.wait(14000)
       //cy.get('.api-card').should('include','Channel Management API');
       cy.contains('Channel Management API').should('be.visible') 
     })
